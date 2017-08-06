00cbb39eec54dc2152bbc0f50b4bf3b4899065b7
Jasper Stam <jasper@codeyellow.nl>
2017-08-06 21:45:20 +0200

    Fix setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env python3
+import os
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='chimera',
+    version='0.0.1',
+    packages=['chimera'],
+    license='MIT',
+    description='A framework for websocket APIs.',
+    author='Jasper Stam',
+    author_email='jasper@strnk.nl',
+    url='https://github.com/JasperStam/chimera',
+    classifiers=[],
+    install_requires=[
+        'gevent >= 1.1.2',
+        'Flask >= 0.12.0',
+        'Flask-Script >= 2.0.5',
+        'Flask-Sockets >= 0.2.1',
+        'Flask-SQLAlchemy >= 2.1',
+        'Flask-Migrate >= 2.0.3',
+        'pyjwt >= 1.4.2',
+        'python-dateutil >= 2.6.0',
+        'python-dotenv >= 0.6.3',
+        'requests >= 2.13.0',
+    ],
+    test_suite='tests'
+)
