28d683f6e0f0c399e5b36665fd400a7f97396d33
Jasper Stam <jasper@codeyellow.nl>
2017-09-10 20:01:22 +0200

    Add initial subscribe testcase

---
+++ tests/test_subscribe.py
@@ -0,0 +1,54 @@
+import json
+from .testapp.app import app, db
+from .testapp.models import Company
+from archon.test import LoggedInTestCase, Client, MockWebSocket
+
+save_company = {
+    'target': 'company',
+    'type': 'save',
+    'data': {
+        'name': 'CY',
+    },
+}
+
+subscribe_company = {
+    'requestId': '1234',
+    'target': 'company',
+    'type': 'subscribe',
+}
+
+
+class TestSubscribe(LoggedInTestCase):
+    def setUp(self):
+        self.client = Client(app, db)
+        super().setUp()
+
+    def test_publishes_existing_collection_upon_subscription(self):
+        ws = MockWebSocket()
+        ws.mock_incoming_message(subscribe_company)
+
+        c = Company({
+                'name': 'Slagerij Henk',
+            })
+        db.session.add(c)
+        db.session.commit()
+
+        self.client.open_connection(ws)
+
+        self.assertEqual(1, len(ws.outgoing_messages))
+        m = ws.outgoing_messages[0]
+
+        self.assertDictEqual({
+            'type': 'publish',
+            'target': 'company',
+            'requestId': '1234',
+            'data': {
+                'add': [{
+                    'id': c.id,
+                    'name': c.name,
+                }],
+                'update': [],
+                'delete': [],
+            },
+            'code': 'success',
+        }, json.loads(m))
