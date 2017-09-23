0bd6ea2072daf46ddd6b44862c7ba1dc9b479f88
Jasper Stam <jasper@codeyellow.nl>
2017-09-23 18:58:05 +0200

    Add test for finding / scoping by fk

--- tests/test_subscribe.py
+++ tests/test_subscribe.py
@@ -1,6 +1,6 @@
 import json
 from .testapp.app import app, db
-from .testapp.models import Company
+from .testapp.models import Company, Employee
 from archon.test import LoggedInTestCase, Client, MockWebSocket
 
 save_company = {
@@ -53,3 +53,63 @@ class TestSubscribe(LoggedInTestCase):
             },
             'code': 'success',
         }, json.loads(m))
+
+
+subscribe_employee_c1 = {
+    'requestId': '5678',
+    'target': 'employee',
+    'scope': {
+        'company': 1,
+    },
+    'type': 'subscribe',
+}
+
+
+class TestScope(LoggedInTestCase):
+    def setUp(self):
+        self.client = Client(app, db)
+        super().setUp()
+
+    def test_fk(self):
+        ws = MockWebSocket()
+
+        c1 = Company({'name': 'Slagerij Henk'})
+        c2 = Company({'name': 'Slagerij Pieter'})
+        db.session.add(c1)
+        db.session.add(c2)
+        e1 = Employee({'name': 'Henk'})
+        e1.company = c1
+        db.session.add(e1)
+        db.session.commit()
+
+        ws.mock_incoming_message(subscribe_employee_c1)
+        ws.mock_incoming_message({
+            'target': 'employee',
+            'type': 'save',
+            'data': {
+                'name': 'Jan',
+                'company': c1.id,
+            },
+        })
+        ws.mock_incoming_message({
+            'target': 'employee',
+            'type': 'save',
+            'data': {
+                'name': 'Pieter',
+                'company': c2.id,
+            },
+        })
+
+        self.client.open_connection(ws)
+        # Subscribe success, publish, save success, save success
+        self.assertEqual(4, len(ws.outgoing_messages))
+        res_sub = json.loads(ws.outgoing_messages[0])
+        res_pub = json.loads(ws.outgoing_messages[1])
+        res_sv1 = json.loads(ws.outgoing_messages[2])
+        res_sv2 = json.loads(ws.outgoing_messages[3])
+
+        for r in [res_sv1, res_sv2]:
+            self.assertEqual('success', r['code'])
+
+        self.assertEqual('Henk', res_sub['data']['add'][0]['name'])
+        self.assertEqual('Jan', res_pub['data']['add'][0]['name'])
